88241555d23ccb545acdcecb8aa34f899ae3dcbe
Néstor González <alu0100108859@ull.edu.es>
2021-11-18 18:35:55 +0000

    todo

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Mi Segundo post",
+        "excerpt":"Daredevil post   Usuario: I am an amazing person.      Notas de clase  Somos 20 alumnos   El resultado de chuchu es 4   Probando estructuras   En breve es fiiindeee!!   Frutitas           Me gusta el melón            Me gusta el coco            Me gusta el higo       ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/post2",
+        "teaser": null
+      },{
+        "title": "Aprendiendo a usar esto",
+        "excerpt":"Mi nombre es: Néstor Fórmula \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Datos Leer CSV — First name Last name Age Location John Doe 35 United States Jane Doe 29 France Jack Hill 25 Australia Primer post «Sabed, oh, príncipe, que entre los años en que los...","categories": ["docentia"],
+        "tags": [],
+        "url": "/docentia/practica3",
+        "teaser": null
+      }]
