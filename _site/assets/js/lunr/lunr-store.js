3c5e85aa12b13841ffe7e5932e1c4fd170698c68
Néstor González <alu0100108859@ull.edu.es>
2021-11-22 11:42:48 +0000

    new-version

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,9 +5,15 @@ var store = [{
         "url": "/blog/post2",
         "teaser": null
       },{
-        "title": "Aprendiendo a usar esto",
+        "title": "Aprendizaje Individual",
         "excerpt":"Mi nombre es: Néstor Fórmula \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Datos Leer CSV — First name Last name Age Location John Doe 35 United States Jane Doe 29 France Jack Hill 25 Australia Primer post «Sabed, oh, príncipe, que entre los años en que los...","categories": ["docentia"],
         "tags": [],
         "url": "/docentia/practica3",
+        "teaser": null
+      },{
+        "title": "Aprendizaje Competitivo",
+        "excerpt":"Mi nombre es: Néstor  ","categories": ["docentia"],
+        "tags": [],
+        "url": "/docentia/practica4",
         "teaser": null
       }]
